e924fb1e90f59dcf79563528831000dc96d74765
Bartosz Galek <bartosz.galek@allegro.pl>
2021-07-16 19:53:12 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -2790,13 +2790,11 @@ const uuid = __nccwpck_require__(840);
 module.exports = function (host, username, password, name, group, version) {
   const temp = uuid.v4();
   const imagetag = `${host}/${group}/${name}`;
+  core.info(exec(`docker login -u ${username} -p ${password} artifactory.allegrogroup.com`).toString());
   core.info(exec(`docker build -t ${imagetag}:${temp} .`).toString());
   core.info(exec(`docker tag ${imagetag}:${temp} ${imagetag}:${version}`).toString());
   core.info(exec('echo \'{"auths": \' >> config.json').toString());
-  core.info(exec(`curl -u ${username}:${password} -s "${host}/v2/auth" >> config.json`).toString());
-  core.info(exec('echo " }" >> config.json').toString());
-  core.info(exec(`sed -i 's%localhost:8081%${host}%' config.json`).toString());
-  core.info(exec(`docker --config=./ push ${imagetag}:${version}`).toString());
+  core.info(exec(`docker push ${imagetag}:${version}`).toString());
 };
 
 /***/ }),
--- packaging/docker.js
+++ packaging/docker.js
@@ -5,11 +5,9 @@ const uuid = require('uuid');
 module.exports = function (host, username, password, name, group, version) {
   const temp = uuid.v4();
   const imagetag = `${host}/${group}/${name}`;
+  core.info(exec(`docker login -u ${username} -p ${password} artifactory.allegrogroup.com`).toString());
   core.info(exec(`docker build -t ${imagetag}:${temp} .`).toString());
   core.info(exec(`docker tag ${imagetag}:${temp} ${imagetag}:${version}`).toString());
   core.info(exec('echo \'{"auths": \' >> config.json').toString());
-  core.info(exec(`curl -u ${username}:${password} -s "${host}/v2/auth" >> config.json`).toString());
-  core.info(exec('echo " }" >> config.json').toString());
-  core.info(exec(`sed -i 's%localhost:8081%${host}%' config.json`).toString());
-  core.info(exec(`docker --config=./ push ${imagetag}:${version}`).toString());
+  core.info(exec(`docker push ${imagetag}:${version}`).toString());
 };
