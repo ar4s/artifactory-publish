809c7581eab2dff70344619890aa0252b518418b
Bartosz Galek <bartosz.galek@allegro.pl>
2021-07-16 16:16:02 +0200

    testing

--- dist/index.js
+++ dist/index.js
@@ -2142,17 +2142,7 @@ const core = __nccwpck_require__(186);
 
 module.exports = function (host, username, password, name, group, version) {
   exec(`curl -u ${username}:${password} -s https://${host}/v2/auth > .dockercfg`);
-  const command = `
-    export IMAGEID=$(uuidgen)
-    export IMAGETAG="${host}/${group}/${name}:${version}"
-    docker build -t $IMAGETAG:$IMAGEID .
-    docker tag $IMAGETAG:$IMAGEID $IMAGETAG:${version}
-    echo '{"auths": ' >> config.json
-    curl -u ${username}:${password} -s "${host}/v2/auth" >> config.json
-    echo " }" >> config.json
-    sed -i 's%localhost:8081%${host}%' config.json
-    docker --config=./ push $IMAGETAG:${version}
-  `.replace(/\n/g, ' \\\n');
+  const command = `IMAGEID=$(uuidgen) IMAGETAG="${host}/${group}/${name}:${version} docker build -t $IMAGETAG:$IMAGEID . && docker tag $IMAGETAG:$IMAGEID $IMAGETAG:${version} && echo '{"auths": ' >> config.json && curl -u ${username}:${password} -s "${host}/v2/auth" >> config.json && echo " }" >> config.json && sed -i 's%localhost:8081%${host}%' config.json && docker --config=./ push $IMAGETAG:${version}`;
   const stdout = exec(command);
   core.info(String(stdout).trim());
 };
