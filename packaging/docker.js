afa4c9a0a3eeda4dbf90cf61f5d426147c605f18
Bartosz Galek <bartosz.galek@allegro.pl>
2021-07-16 16:18:17 +0200

    testing

--- packaging/docker.js
+++ packaging/docker.js
@@ -3,7 +3,7 @@ const core = require('@actions/core');
 
 module.exports = function (host, username, password, name, group, version) {
   exec(`curl -u ${username}:${password} -s https://${host}/v2/auth > .dockercfg`);
-  const command = `IMAGEID=$(uuidgen) IMAGETAG="${host}/${group}/${name}:${version} docker build -t $IMAGETAG:$IMAGEID . && docker tag $IMAGETAG:$IMAGEID $IMAGETAG:${version} && echo '{"auths": ' >> config.json && curl -u ${username}:${password} -s "${host}/v2/auth" >> config.json && echo " }" >> config.json && sed -i 's%localhost:8081%${host}%' config.json && docker --config=./ push $IMAGETAG:${version}`;
+  const command = `IMAGEID=$(uuidgen) IMAGETAG="${host}/${group}/${name}:${version}" docker build -t $IMAGETAG:$IMAGEID . && docker tag $IMAGETAG:$IMAGEID $IMAGETAG:${version} && echo '{"auths": ' >> config.json && curl -u ${username}:${password} -s "${host}/v2/auth" >> config.json && echo ' }' >> config.json && sed -i 's%localhost:8081%${host}%' config.json && docker --config=./ push $IMAGETAG:${version}`;
   const stdout = exec(command);
   core.info(String(stdout).trim());
 };
